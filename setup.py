8afd17709ccbaa426728e5c14fd29313b0f9b2b4
taka9999 <to0t0seki@gmail.com>
2024-11-06 21:03:10 +0900

    絶対パスについての修正

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name="geek_token_analytics",
     packages=find_packages(),
     version="0.1.0",
+    install_requires=[
+        "streamlit",
+        "plotly",
+        "pandas",
+        "web3",
+        "python-decouple",
+        "streamlit-aggrid",
+    ],
+    python_requires=">=3.8",
 )
