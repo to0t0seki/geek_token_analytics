a7ebf5b6c9081fccc32a6c16e1e847d0bb5bdc5f
taka9999 <to0t0seki@gmail.com>
2024-11-06 22:51:31 +0900

    git rm -r --cached . の実行

--- scripts/df_viewer.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from src.data_access.database import get_daily_xgeek_to_geek
-
-df = get_daily_xgeek_to_geek("data/processed/geek_transfers.db")
-print(df.info())
-print(df.head())
