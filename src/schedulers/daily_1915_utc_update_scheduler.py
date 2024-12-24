6390a23ebe98f15c01970c4b7d3ac52f765f6df1
taka9999 <to0t0seki@gmail.com>
2024-12-25 00:21:40 +0900

    修正

--- src/schedulers/daily_1915_utc_update_scheduler.py
+++ src/schedulers/daily_1915_utc_update_scheduler.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("~/geek_analytics_test")
+sys.path.append("/home/ubuntu/geek_analytics_test")
 from src.data_processing.adjusted_daily_balances_calculator import calculate_yesterday_balances
 from src.data_collection.market_data_importer import import_market_data
 
