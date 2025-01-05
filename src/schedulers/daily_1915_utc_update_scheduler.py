15eff5789d26e3ba4933352d453304316d8ceb9f
taka9999 <to0t0seki@gmail.com>
2025-01-06 07:48:38 +0900

    1905 to 1915

--- src/schedulers/daily_1915_utc_update_scheduler.py
+++ src/schedulers/daily_1915_utc_update_scheduler.py
@@ -3,15 +3,15 @@ sys.path.append("/home/ubuntu/geek_analytics_test")
 from src.data_processing.adjusted_daily_balances_calculator import calculate_yesterday_balances
 from datetime import datetime, timedelta
 
-def daily_1905_utc_update_scheduler():
+def daily_1915_utc_update_scheduler():
     current_time = datetime.now() + timedelta(hours=9)
     print(f"start: {current_time.strftime('%Y-%m-%d %H:%M:%S')}")
-    print("1905 UTCの更新を行います")
+    print("1915 UTCの更新を行います")
     calculate_yesterday_balances()
-    print("1905 UTCの更新を行いました")
+    print("1915 UTCの更新を行いました")
 
 if __name__ == "__main__":
-    daily_1905_utc_update_scheduler()
+    daily_1915_utc_update_scheduler()
 
 
 
