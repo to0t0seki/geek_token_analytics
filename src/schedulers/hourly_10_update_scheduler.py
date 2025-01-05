189ae27d15ba404d590b9b07f884a100c70e4be5
taka9999 <to0t0seki@gmail.com>
2025-01-05 20:18:53 +0900

    change sidever time

--- src/schedulers/hourly_10_update_scheduler.py
+++ src/schedulers/hourly_10_update_scheduler.py
@@ -1,7 +1,9 @@
 from src.data_collection.geek_transaction_importer import get_geek_data, calculate_todays_balances
+from datetime import datetime, timedelta
 
 def hourly_10_update_scheduler():
-    print("10分の更新を行います")
+    current_time = datetime.now() + timedelta(hours=9)
+    print(f"start: {current_time.strftime('%Y-%m-%d %H:%M:%S')}")
     get_geek_data()
     calculate_todays_balances()
     print("10分の更新を行いました")
