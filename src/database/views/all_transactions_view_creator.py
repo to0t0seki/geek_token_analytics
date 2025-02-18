9123663573f6b1532f7d81cf0594878d850d0709
taka9999 <to0t0seki@gmail.com>
2025-02-18 10:48:10 +0900

    vw_all_transactionsの作成

--- src/database/views/all_transactions_view_creator.py
+++ src/database/views/all_transactions_view_creator.py
@@ -1,26 +1,54 @@
 from src.database.data_access.database_client import DatabaseClient
 
-def create_vw_all_transactions() -> None:
+def create_vw_all_transactions(client: DatabaseClient) -> None:
     """all_transactionsビューを作成する"""
-    client = DatabaseClient()
-    try:
-        # 既存のビューが存在する場合は削除
-        client.execute("DROP VIEW IF EXISTS all_transactions")
-        
-        # ビューの作成
-        create_view = """
-        CREATE VIEW all_transactions AS
-        SELECT * 
-        FROM geek_transactions 
-        UNION ALL
-        SELECT *
-        
-        """
-        client.execute(create_view)
-        print("all_transactionsビューを作成しました。")
-        
-    except Exception as e:
-        print(f"エラーが発生しました: {e}")
+   
+    client.execute("DROP VIEW IF EXISTS vw_all_transactions")
+    
+    # ビューの作成
+    create_view = """
+    CREATE VIEW vw_all_transactions AS
+    SELECT 
+        block_number,
+        log_index,
+        tx_hash,
+        timestamp,
+        from_address,
+        to_address,
+        value AS unified_amount,
+        method,
+        type,
+        'geek' AS source
+    FROM geek_transactions
+    UNION ALL
+    SELECT 
+        block_number,
+        log_index,
+        tx_hash,
+        timestamp,
+        from_address,
+        to_address,
+        token_id AS unified_amount,
+        method,
+        type,
+        'doll' AS source
+    FROM doll_transactions
+    UNION ALL
+    SELECT 
+        block_number,
+        log_index,
+        tx_hash,
+        timestamp,
+        from_address,
+        to_address,
+        token_id AS unified_amount,
+        method,
+        type,
+        'equipment' AS source
+    FROM equipment_transactions;
+    """
+    client.execute(create_view)
+    print("vw_all_transactionsを作成しました。")
 
 if __name__ == "__main__":
-    create_airdrops_view()
+    create_vw_all_transactions(DatabaseClient())
